52f00c9df7d58beb0790645c6d5fed95c2dfe578
jossefe <youseflaknaizi@gmail.com>
2024-02-20 12:54:28 +0100

    edit card

--- router/src/main.jsx
+++ router/src/main.jsx
@@ -5,23 +5,32 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import "./index.css";
-import Root from "./routes/root";
 import ErrorPage from "./error-page";
-import Contact from "./routes/contact";
-
+import Contact, { loader as contactLoader,}from "./routes/contact";
+import Root, { loader as rootLoader,  action as rootAction,} from "./routes/root";
+import EditContact from "./routes/edit";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
     children: [
       {
         path: "contacts/:contactId",
         element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
       },
     ],
   },
 ]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
--- router/src/routes/contact.jsx
+++ router/src/routes/contact.jsx
@@ -1,14 +1,12 @@
-import { Form } from "react-router-dom";
 
+import { Form, useLoaderData } from "react-router-dom";
+import { getContact } from "../contacts";
+export async function loader({ params }) {
+  const contact = await getContact(params.contactId);
+  return { contact };
+}
 export default function Contact() {
-  const contact = {
-    first: "Your",
-    last: "Name",
-    avatar: "https://placekitten.com/g/200/200",
-    twitter: "your_handle",
-    notes: "Some notes",
-    favorite: true,
-  };
+  const { contact } = useLoaderData();
 
   return (
     <div id="contact">
@@ -69,7 +67,7 @@ export default function Contact() {
   );
 }
 
-function Favorite({contact}) {
+function Favorite({ contact }) {
   // yes, this is a `let` for later
   let favorite = contact.favorite;
   return (
